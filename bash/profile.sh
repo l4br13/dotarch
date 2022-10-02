118ae911774153bd55ae6686b292536d74431de8
l4br13 <t2051pz@gmail.com>
2022-10-02 23:59:32 +0700

    Sun Oct  2 11:59:32 PM WIB 2022

--- bash/profile.sh
+++ bash/profile.sh
@@ -1,8 +1,7 @@
 # startx
-[[ $(fgconsole 2>/dev/null) == 1 ]] && exec startx -- vt1 &> /dev/null
 if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-	if [ -f ~/.Xauthority ]: then
+	if [ -f ~/.Xauthority ]; then
 		rm -rf ~/.Xauthority
 	fi
-	exec startx
+	[[ $(fgconsole 2>/dev/null) == 1 ]] && exec startx -- vt1 &> /dev/null
 fi
